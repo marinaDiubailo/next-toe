40a1b1086ce88cdf0c389cd173ce8aafcac1b74a
MarinaDiubailo <marinadiubailo@gmail.com>
2024-11-25 12:34:44 +0300

    feat: add prisma seed, prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { prisma } from '@/shared/lib/db/db';
 import { HomePage } from '@pages/home';
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log(games);
   return <HomePage />;
 }
